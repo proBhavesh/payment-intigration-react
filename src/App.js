d848f00cad3f50431d3cffe430d2f9f912f98d74
Bhavesh <bhaveshjat9950@gmail.com>
2021-04-15 10:39:39 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
-const App = ()=>{
-  return <h1>This is App</h1>
-}
+const App = () => {
+	return <h1 className="btn">This is App</h1>;
+};
 
 export default App;
